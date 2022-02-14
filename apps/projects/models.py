3503aaede8d371332c7f4c23769e0f401ddeae8f
Elias Guere <eliasguere1025@gmail.com>
2022-02-13 23:04:07 -0500

    almost finish page

--- apps/projects/models.py
+++ apps/projects/models.py
@@ -14,6 +14,7 @@ class Technology(models.Model):
 class Experience(models.Model):
     job = models.CharField(max_length=200)
     company = models.CharField(max_length=200)
+    description = models.TextField(null=True)
     init_date = models.DateField()
     finish_date = models.DateField(null=True)
 
